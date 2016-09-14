a443739b77bd0fc1ad29ca17a514ad96b1c7158c
Jack Morrison <jack1morrison@sky.com>
2016-09-14 11:22:18 +0100

    Initial Commit

---
+++ iMaths/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  iMaths
+//
+//  Created by Jack Morrison on 14/09/2016.
+//  Copyright © 2016 Jack Morrison. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
